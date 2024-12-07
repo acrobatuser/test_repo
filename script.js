bac960e765e1bd9eade1c0dbe6009d1dc7be61e9
acrobatuser <acrobatuser12345@tutanota.com>
2024-12-06 22:22:01 -0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // jsonData is directly accessible from json-data.js
-    document.getElementById('jsonData').textContent = JSON.stringify(jsonData, null, 2);
-    console.log('hello world');
-});
